b44dc8889f23797d493a7ca58885e5ca2c6887ac
César Guerra <warderer@gmail.com>
2024-04-23 19:43:48 -0600

    🎉 Express JS API Server project init

---
+++ 01-intro-express/server.js
@@ -0,0 +1,15 @@
+// #1 Llamar a la biblioteca de express
+const express = require('express'); // Common.JS
+
+// #2 Crear una instancia de express
+const app = express();
+
+app.use(express.urlencoded({ extended: true })); // urlencoded me permite recibir información más rica en el body
+app.use(express.json()); // json me permite recibir información en formato json
+
+// #3 Definir las rutas
+
+// #4 Levantar el servidor en un puerto, por ejemplo el 3000
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000 🚀');
+});
