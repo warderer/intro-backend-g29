f690c13495d30bbd5d1e3bee50eadfea098d427f
César Guerra <warderer@gmail.com>
2024-04-23 20:48:52 -0600

    ✨ Add getOnePet Route

--- 01-intro-express/api/v1/pets.js
+++ 01-intro-express/api/v1/pets.js
@@ -28,4 +28,17 @@ router.get('/api/v1/pets', (request, response) => {
     response.send(petList);
 });
 
+/* PARAMS */
+// Obtener parámetros de la URL (dinámicos)
+// Params: '/api/v1/pets/:petId'
+router.get('/api/v1/pets/:petId', (request, response) => {
+    console.log('Params de GetOnePet:', request.params.petId);
+    // recorrer el arreglo de pets y devolver el que coincida con el id
+    const onePet = petList.pets.find(pet => pet.id === parseInt(request.params.petId)); // Si la ID del pet existe, devolvera un objeto (true), si no, devolvera undefined (false)
+
+    onePet
+      ? response.status(200).send(onePet)
+      : response.status(404).send({ message: 'Pet not found'});
+});
+
 module.exports = router;
