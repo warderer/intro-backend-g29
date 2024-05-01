96e6af5626597fb7e09058db92eed54be5210897
César Guerra <warderer@gmail.com>
2024-04-30 20:13:53 -0600

    ✨ Add MVC createHome

--- 02-knex-api-g29a/controllers/homeController.js
+++ 02-knex-api-g29a/controllers/homeController.js
@@ -1,9 +1,17 @@
 /* Los controladores tienen la logica de negocio */
+const modelHomes = require('../models/Homes')
 
 // CREATE
 const createHome = (req, res) => {
   // Aquí yo debería crear el home
-  res.send({ message: 'Hola Mundo! 🌍' })
+  // res.send({ message: 'Hola Mundo! 🌍' })
+  modelHomes.create(req.body)
+    .then(home => {
+      res.status(201).send(home)
+    })
+    .catch(error => {
+      res.status(400).send(error)
+    })
 }
 
 // READ
---
+++ 02-knex-api-g29a/models/Homes.js
@@ -0,0 +1,18 @@
+// El modelo trae los datos de la base de datos que
+// NO se encarga de validar datos, ni resolver promesas, eso es trabajo del controlador en MVC
+
+// Paso #1 Necesito importar la configuración del entorno de knex y los detalles de la conexión a la base de datos que
+const knex = require('../config')
+
+// Paso #2 Crear una función que traiga los datos que yo requiera de la base de datos
+const create = (bodyHome) => {
+  return knex
+    .insert(bodyHome) // ¿Qué datos voy a insertar? { title: 'titulo', description: 'descripcion'}
+    .into('homes') // ¿En que tabla? - homes
+    .returning('*') // ¿Qué datos quiero que me regrese? - todos los datos
+}
+
+// Paso #3 Exportar la función para que pueda ser usada en otro archivo
+module.exports = {
+  create
+}
